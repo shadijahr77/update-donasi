01f8f1111f47488719166522c83a173979d13f9f
shadijahr77 <shadijahr77@gmail.com>
2026-02-23 18:04:08 +0800

    first upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Target donasi
+const targetDonasi = 2500000;
+
+// Data donasi masuk (EDIT DI SINI)
+const donasiMasuk = [
+  { nama: "Ahmad", jumlah: 500000 },
+  { nama: "Hamba Allah", jumlah: 25000000 },
+  { nama: "Siti", jumlah: 2000000 },
+  { nama: "Alfian", jumlah: 500000}
+];
+
+let total = 0;
+
+const totalElement = document.getElementById("total");
+const progressElement = document.getElementById("progress");
+const listElement = document.getElementById("listDonasi");
+
+donasiMasuk.forEach(donasi => {
+  total += donasi.jumlah;
+
+  const li = document.createElement("li");
+  li.textContent = `${donasi.nama} - Rp ${donasi.jumlah.toLocaleString("id-ID")}`;
+  listElement.appendChild(li);
+});
+
+// Update total
+totalElement.textContent = "Rp " + total.toLocaleString("id-ID");
+
+// Update progress bar
+const persen = (total / targetDonasi) * 100;
+progressElement.style.width = persen + "%";
